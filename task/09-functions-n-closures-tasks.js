0444e1a948c0cd40a792821d303ffcdc0a7f0177
Yaroslav Levotsky <44747072+Saik1s@users.noreply.github.com>
2019-09-18 23:43:01 +0300

    add 'getComposition' and 'getPowerFunction' tasks

--- task/09-functions-n-closures-tasks.js
+++ task/09-functions-n-closures-tasks.js
@@ -26,7 +26,9 @@
  *
  */
 function getComposition(f,g) {
-    throw new Error('Not implemented');
+    return (x) => {
+        f(g(x));
+    };
 }
 
 
@@ -47,7 +49,9 @@ function getComposition(f,g) {
  *
  */
 function getPowerFunction(exponent) {
-    throw new Error('Not implemented');
+    return (x) => {
+        return Math.pow(x, exponent);
+    };
 }
 
 
