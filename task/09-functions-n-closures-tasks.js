b16834cf82eb39dbcbd486993bceda34058cfaab
Yaroslav Levotsky <44747072+Saik1s@users.noreply.github.com>
2019-09-19 23:01:12 +0300

    add 'memorize' task

--- task/09-functions-n-closures-tasks.js
+++ task/09-functions-n-closures-tasks.js
@@ -95,7 +95,9 @@ function getPolynom() {
  *   memoizer() => the same random number  (next run, returns the previous cached result)
  */
 function memoize(func) {
-    throw new Error('Not implemented');
+    const cache = func();
+    
+    return () => cache;
 }
 
 
